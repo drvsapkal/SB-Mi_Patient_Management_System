8bf4947de7d0ddf2929f1430b29b1d55b01c4362
VDRohit <sapkalrohit110@gmail.com>
2024-03-09 20:40:02 +0530

    "Created Handler method for validation doctor login"

--- Doctor_Login_Registry/src/main/java/com/hospital/controller/DoctorController.java
+++ Doctor_Login_Registry/src/main/java/com/hospital/controller/DoctorController.java
@@ -1,18 +1,21 @@
 package com.hospital.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hospital.entity.Doctor;
+import com.hospital.response_result.ResponseResult;
 import com.hospital.service.Doctor_InterfaceService;
 
 @Controller
+@CrossOrigin("*")
 @RequestMapping("/doctor")
 public class DoctorController {
 
@@ -29,13 +32,13 @@ public class DoctorController {
 		}
 	}
 	
-	@GetMapping("/login")
-	private ResponseEntity<String> doctorSignIn(@RequestBody Doctor doctor) {
-		if (doctor_InterfaceService.doctorSignIn(doctor)) {
-			return new ResponseEntity<String>("Login Success", HttpStatusCode.valueOf(200));
-		} else {
-			return new ResponseEntity<String>("Please Enter Valid Credentials", HttpStatusCode.valueOf(400));
-		}
+	@PostMapping("/login")
+	private ResponseEntity<ResponseResult> doctorSignIn(@RequestBody Doctor doctor) {
+		boolean signInResult = doctor_InterfaceService.doctorSignIn(doctor);
+		String message = signInResult ? "Login Success" : "Invalid username or password";
+		ResponseResult response = new ResponseResult(signInResult, message);
+		HttpStatus status = signInResult ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+		return new ResponseEntity<ResponseResult>(response, status);
 	}
 
 }
