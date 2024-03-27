aa8034004857c516a7439a12ace5601c6419c13c
VDRohit <sapkalrohit110@gmail.com>
2024-03-27 17:18:06 +0530

    Corrected url Reponse Converted into Json Object so FrontEnd Should get message data saved or not

--- Doctor_Login_Registry/src/main/java/com/hospital/controller/DrugController.java
+++ Doctor_Login_Registry/src/main/java/com/hospital/controller/DrugController.java
@@ -1,17 +1,20 @@
 package com.hospital.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hospital.entity.Drug;
+import com.hospital.response_result.ResponseResult;
 import com.hospital.service.Drug_InterfaceService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/drug")
 public class DrugController {
 
@@ -19,15 +22,13 @@ public class DrugController {
 	Drug_InterfaceService drug_InterfaceService;
 	
 	@PostMapping("/addPatientDrug")
-	private ResponseEntity<Drug> addDrugs(@RequestBody Drug drug) {
+	private ResponseEntity<ResponseResult> addDrugs(@RequestBody Drug drug) {
 		
-		if(drug_InterfaceService.addDrugs(drug))
-		{
-			return new ResponseEntity<Drug>(HttpStatusCode.valueOf(200));
-		}
-		else {
-			return new ResponseEntity<Drug>(HttpStatusCode.valueOf(401));
-		}
-	}
+		boolean addedMedicines = drug_InterfaceService.addDrugs(drug);
+		String message = addedMedicines ? "Added Medicine to Patient" : "Please Enter Valid Medicine";
+		ResponseResult response = new ResponseResult(addedMedicines, message);
+		HttpStatus status = addedMedicines ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
+		return new ResponseEntity<ResponseResult>(response, status);
 	
+	}	
 }
